f8d6d042c3da12586041ebbd48c2155ed691434c
Akshat Bhanchawat <akshat112@gmail.com>
2020-03-23 17:23:04 +0530

    https resolve

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://tttonline.herokuapp.com');
+var socket = io.connect('https://tttonline.herokuapp.com');
 // var socket = io.connect('http://localhost:3000');
 
 function el(id){
