541a14d9a34852584c69b2c685f66e3b19846e76
Akshat Bhanchawat <36301928+akshat112@users.noreply.github.com>
2023-12-26 13:19:40 +0530

    Update main.js
    URL edits

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('tictactoe-6bwp.onrender.com');
+var socket = io.connect('https://tictactoe-6bwp.onrender.com');
 console.log(process.ENV.URL)
 // var socket = io.connect('http://localhost:3000');
 
